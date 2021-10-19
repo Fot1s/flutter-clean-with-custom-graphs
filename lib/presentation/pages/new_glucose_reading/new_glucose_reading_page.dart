d7c00129f61f4969e2e39ce7b53ab791b69f9f9b
Fotis Chatzinikos <fotis.chatzinikos@gmail.com>
2021-10-19 04:31:12 +0300

    StreamViewModel does updates internally no need for StreamBuilder in the UI side

--- lib/presentation/pages/new_glucose_reading/new_glucose_reading_page.dart
+++ lib/presentation/pages/new_glucose_reading/new_glucose_reading_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lab_coffee/core/domain/data_point.dart';
 import 'package:lab_coffee/presentation/pages/new_glucose_reading/new_glucose_reading_page_view_model.dart';
 import 'package:lab_coffee/presentation/widgets/glucose_activity_card.dart';
 import 'package:stacked/stacked.dart';
@@ -19,16 +18,7 @@ class NewGlucoseReadingPage extends StatelessWidget {
         appBar: AppBar(
           title: Text(viewModel.title),
         ),
-        body:
-        StreamBuilder<List<DataPoint>>(
-          stream: viewModel.stream,
-          builder: (BuildContext context, AsyncSnapshot<List<DataPoint>> snapshot) {
-            return snapshot.data?.isNotEmpty == true ?
-              GlucoseActivityCard(viewModel.title, "Avg ${viewModel.averageReading.toStringAsFixed(2)} mg/dl", snapshot.data, false, viewModel.startTime, viewModel.endTime)
-              :
-              const Center(child:CircularProgressIndicator()) ;
-          },
-        ),
+        body:GlucoseActivityCard(viewModel.title, "Avg ${viewModel.averageReading.toStringAsFixed(2)} mg/dl", viewModel.glucoseData, false, viewModel.startTime, viewModel.endTime),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.red,
           child: const Icon(Icons.stop),
